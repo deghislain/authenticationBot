c33fbedc5f02e176b5d489f9ec0b9fce9b6143f6
deghislain <deghislain@gmail.com>
2024-09-06 15:46:44 -0300

    code clean up

--- service_info_extractor.py
+++ service_info_extractor.py
@@ -1,9 +1,23 @@
 from langchain_nvidia_ai_endpoints import ChatNVIDIA
-import utilities as util
+import streamlit as st
+
+
+def get_the_conversation():
+    chat_history = st.session_state['chat_history']
+    index = 0
+    conversation = ""
+    if len(chat_history) >= 5:
+        for msg in chat_history:
+            if index % 2 == 0:
+                conversation += "user: " + msg + "\n"
+            else:
+                conversation += "assistant: " + msg + "\n"
+            index += 1
+    return conversation
 
 
 def get_service_info():
-    conversation = util.get_the_conversation()
+    conversation = get_the_conversation()
     print(conversation)
     service_name = ""
     if conversation != "":
